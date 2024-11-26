3375f43abab31bd2cdaebdf967260e993acde567
Minimax Femto <charlemax10@gmail.com>
2024-11-26 11:00:11 +0100

    commit pour mettre le rgb.h

--- RGB/rgb.cpp
+++ RGB/rgb.cpp
@@ -1,8 +1,16 @@
+#include "rgb.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-
+	UColor colorVal;
+	cout << "Entrez les couleurs: " << endl;
+	cout << "r: 0x";
+	cin >> hex >> colorVal.components.r;
+	cout << "g: 0x";
+	cin >> hex >> colorVal.components.g;
+	cout << "b: 0x";
+	cin >> hex >> colorVal.components.b;
 }
---
+++ RGB/rgb.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+struct Srgb
+{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+union UColor
+{
+	unsigned int val;
+	Srgb components;
+	unsigned char tabCol[3];
+};
